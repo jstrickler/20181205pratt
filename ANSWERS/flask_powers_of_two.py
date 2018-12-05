4fa9e441f771e9e6c808cff7fb1b9d67470cfc98
jstrickler <jstrickler@gmail.com>
2018-12-05 12:02:20 -0500

    after chapter 2

--- ANSWERS/flask_powers_of_two.py
+++ ANSWERS/flask_powers_of_two.py
@@ -5,15 +5,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     page = '<h1>Powers of two </h1>\n'
     for i in range(32):
-        line = '2^{} = {}<br/>\n'.format(i, 2**i)
-        page += line
+        page += f'<b>2^{i} = {2**i}</b><br/>\n'
     return page
-
 
 if __name__ == '__main__':
     app.run(debug=True)
+z`
--- EXAMPLES/flask_hello.py
+++ EXAMPLES/flask_hello.py
@@ -4,10 +4,17 @@ from flask import Flask
 app = Flask(__name__)  # <1>
 
 
-@app.route('/')  # <2>
-def index():  # <3>
-    return '<h1>Hello, Flask world!</h1>' # <4>
+@app.route('/')  # <2>  DECORATOR
+def index():  # <3> # view function
+    return '<h1>Hello, Pratt & Whitney Flask world!</h1>' # <4>
 
+@app.route('/koala/nesting/pictures/hires')
+@app.route('/wombat')  # <2>  DECORATOR
+def wombats():  # view function
+    name = 'Bob'
+    return f'I like wombats<br/> and koalas! My name is {name}' # <4>
+
+# index = app.route('/')(index)
 
 if __name__ == '__main__':
-    app.run(debug=True) # <5>
+    app.run(debug=True, port=5001) # <5>
